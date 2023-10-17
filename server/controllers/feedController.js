b2008d224db6db0e44c16049454ea8aa6f615c11
Tyler Shelton <tdshelton@gmail.com>
2023-10-17 15:08:28 -0700

    Refactor `.subscribe()` to get out of promise hell

--- server/controllers/feedController.js
+++ server/controllers/feedController.js
@@ -1,20 +1,16 @@
 const Feed = require('../models/feed');
 
 module.exports = {
-  subscribe: (req, res, next) => {
-    import('@extractus/feed-extractor')
-      .then(rss => {
-        rss.extract(req.body.feedURL)
-          .then(feed => {
-            Feed.create(req.body.feedURL, feed.title, feed.description)
-              .then(dbResult => {
-                console.log(dbResult);
-                next();
-              })
-              .catch(err => next(err));
-          })
-          .catch(err => next(err));
-      }).catch(err => next(err));
+  subscribe: async (req, res, next) => {
+    try {
+      const rss  = await import('@extractus/feed-extractor');
+      const feed = await rss.extract(req.body.feedURL);
+      await Feed.create(req.body.feedURL, feed.title, feed.description);
+      next();
+    }
+    catch (err) {
+      next(err);
+    }
   },
 
   unsubscribe: (req, res, next) => {
