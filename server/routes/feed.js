f0b176df906db88ce675d08fea5bdb29581a2a94
Tyler Shelton <tdshelton@gmail.com>
2023-10-18 13:16:35 -0700

    Have Feed routes return the result of their work back to the client

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -15,17 +15,17 @@ router.get('/:id', feedController.getFeed, (req, res) => {
 
 // subscribe to a new feed
 router.post('/subscribe', feedController.subscribe, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send(res.locals.feed);
 });
 
 // unsubscribe from a feed
 router.delete('/:id', feedController.unsubscribe, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send({ success: true });
 });
 
 // update info about a feed. updating the URL is not allowed
 router.put('/:id', feedController.updateFeed, (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send(res.locals.feed);
 });
 
 module.exports = router;
