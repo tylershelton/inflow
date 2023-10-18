b4a19448f744fc3fb6067689543ea8fe63b6defb
Tyler Shelton <tdshelton@gmail.com>
2023-10-18 09:53:46 -0700

    Reorder table declaration to ensure FKs reference PKs that already exist

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -1,12 +1,12 @@
+CREATE TABLE IF NOT EXISTS category (
+    id                   SERIAL PRIMARY KEY,
+    name    varchar(80)  NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS feed (
     id                       SERIAL PRIMARY KEY,
     url         varchar(255) NOT NULL UNIQUE,
     title       varchar(255) NOT NULL,
     description varchar(255),
     category_id integer      REFERENCES category(id)
-);
-
-CREATE TABLE IF NOT EXISTS category (
-    id                   SERIAL PRIMARY KEY,
-    name    varchar(80)  NOT NULL
 );
