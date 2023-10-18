8ac9a5aadb1ac522024004073ac4616b66f94cfb
Tyler Shelton <tdshelton@gmail.com>
2023-10-17 17:45:25 -0700

    Comment routes

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -3,22 +3,27 @@ const router  = express.Router();
 
 const feedController = require('../controllers/feedController');
 
+// get all feeds
 router.get('/', feedController.getFeeds, (req, res) => {
   return res.status(200).json(res.locals.feeds);
 });
 
+// get a specific feed
 router.get('/:id', feedController.getFeed, (req, res) => {
   return res.status(200).json(res.locals.feed);
 });
 
+// subscribe to a new feed
 router.post('/subscribe', feedController.subscribe, (req, res) => {
   return res.status(200).send();
 });
 
+// unsubscribe from a feed
 router.post('/unsubscribe', feedController.unsubscribe, (req, res) => {
   return res.status(200).send();
 });
 
+// update info about a feed. updating the URL is not allowed
 router.put('/:id', feedController.updateFeed, (req, res) => {
   return res.status(200).send();
 });
