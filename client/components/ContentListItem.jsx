d09ee158f7abae2e08810ca3b216bd795152a3fc
Tyler Shelton <tdshelton@gmail.com>
2023-10-19 13:51:17 -0700

    Feed links emit an event to load the feed browser

--- client/components/ContentListItem.jsx
+++ client/components/ContentListItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import eventBus from '../lib/eventBus';
 
 const ContentListItem = ({ id, title }) => {
+  function handleClick (e) {
+    e.preventDefault();
+    eventBus.dispatch('browseFeed', e.target.href);
+  }
+
   return (
-    <li data-id={id}>{title}</li>
+    <li data-id={id}><a onClick={handleClick} href={`/feeds/${id}`}>{title}</a></li>
   );
 };
 
