5755086244ed7831b0590f70bd09d6b73cfca7d0
Tyler Shelton <tdshelton@gmail.com>
2023-10-19 13:51:57 -0700

    App listens for event to handle loading up a feed browser UI

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+// core
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
+// internal libraries
+import eventBus from './lib/eventBus';
+
+// components
 import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
 import styles from './stylesheets/application.scss'; // webpack will pick this up
 
 const App = () => {
+  const [currFeedUrl, setCurrFeedUrl] = useState('');
+  const [currFeed, setCurrFeed] = useState();
+
+  useEffect(() => {
+    if (!currFeedUrl.length) return;
+    async function getFeedItems () {
+      const data = await fetch(currFeedUrl);
+      const feed = await data.json();
+      setCurrFeed(feed);
+    }
+    getFeedItems();
+  }, [currFeedUrl]);
+
+  eventBus.on('browseFeed', (url) => {
+    setCurrFeedUrl(url);
+  });
+
+  let feed;
+  if (currFeed) {
+    feed = (<Feed
+      id = {currFeed.id}
+      title = {currFeed.title}
+    />);
+  }
+
   return (
-    <Sidebar />
+    <div>
+      <Sidebar />
+      {feed}
+    </div>
   );
 };
 
