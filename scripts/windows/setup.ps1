38f614e20b6bb7c258d87b177407bf981ac4584d
Tyler Shelton <tdshelton@gmail.com>
2024-06-09 22:26:30 -0700

    Use existing dev image for webpack building

--- scripts/windows/setup.ps1
+++ scripts/windows/setup.ps1
@@ -2,8 +2,19 @@
 #                    back to the initial first unused state.
 
 # Execute the bootstrap script
-& "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\bootstrap.ps1"
+# & "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\bootstrap.ps1"
+$originalDir = Get-Location
 
 Set-Location -Path (git rev-parse --show-toplevel)
 
 # Build webpack artifacts
+docker build --quiet -t inflow-dev:latest -f .\Dockerfile.dev .
+# addition of -it here preserves STDOUT colors by convincing the
+# process it's running in an interactive shell (even though we bin
+# the container immediately upon completion)
+docker run --rm -it `
+    -v .:/usr/src/app `
+    -v /usr/src/app/node_modules `
+    inflow-dev npm run build
+
+Set-Location -Path $originalDir
