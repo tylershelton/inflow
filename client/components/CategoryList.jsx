8dfaa4c416d5f08dc0c09ee065a4426154f6e928
Tyler Shelton <tdshelton@gmail.com>
2024-07-01 16:58:28 -0700

    Prop drill the loader's new category contents data

--- client/components/CategoryList.jsx
+++ client/components/CategoryList.jsx
@@ -5,9 +5,10 @@ import Category from './Category';
 export default function CategoryList ({ categories }) {
   const categoryComponents = categories.map((cat, i) => {
     return (<Category
-      key   = {`cat-${i}`}
-      catid = {cat.id}
-      title = {cat.title}
+      key      = {`cat-${i}`}
+      catid    = {cat.id}
+      title    = {cat.title}
+      contents = {cat.contents}
     />);
   });
 
