b62042b2ccc8dd51ce7781f28539dac7172ceab2
Tyler Shelton <tdshelton@gmail.com>
2024-07-01 16:57:12 -0700

    Extend loader to source data for category contents

--- client/components/Root.jsx
+++ client/components/Root.jsx
@@ -1,48 +1,23 @@
 // dependencies
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet, useLoaderData } from 'react-router-dom';
 
-// internal libraries
-// import eventBus from '../lib/eventBus';
-
 // internal components
-// import Article from './Article';
 import Sidebar from './Sidebar';
 
 export async function loader () {
-  return fetch('/categories');
+  const data = await fetch('/categories');
+  const categories = await data.json();
+  // populate each category with its feeds/contents
+  for (const i in categories) {
+    const data = await fetch(`/categories/${categories[i].id}/contents`);
+    categories[i].contents = await data.json();
+  }
+  return categories;
 }
 
 export default function Root () {
   const categories = useLoaderData();
-  
-  // const [currFeedItemUrl, setCurrFeedItemUrl] = useState();
-  // const [currFeedItem, setCurrFeedItem] = useState();
-
-  // eventBus.on('openFeedItem', (url) => {
-  //   setCurrFeedItemUrl(url);
-  // });
-
-  // get data for the article view from the server
-  // useEffect(() => {
-  //   if (!currFeedItemUrl) return;
-  //   async function getFeedItem () {
-  //     const data = await fetch(currFeedItemUrl);
-  //     const feeditem = await data.json();
-  //     setCurrFeedItem(feeditem);
-  //   }
-  //   getFeedItem();
-  // }, [currFeedItemUrl]);
-
-  // let article;
-  // if (currFeedItem) {
-  //   article = (<Article
-  //     title = {currFeedItem.title}
-  //     description = {currFeedItem.description}
-  //     pubdate = {currFeedItem.pubdate}
-  //     url = {currFeedItem.url}
-  //   />);
-  // }
 
   return (
     <>
