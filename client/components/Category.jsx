26e1a5bcd0520c698360f6ec079058876fc5be63
Tyler Shelton <tdshelton@gmail.com>
2024-07-01 16:59:04 -0700

    Use data from Route loader to populate element

--- client/components/Category.jsx
+++ client/components/Category.jsx
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import ContentList from './ContentList';
 
-export default function Category ({ catid, title }) {
-
-  const [contents, setContents] = useState();
-
-  useEffect(() => {
-    async function getContents () {
-      const data = await fetch(`/categories/${catid}/contents`);
-      const contents = await data.json();
-      setContents(contents);
-    }
-    getContents();
-  }, []);
-
+export default function Category ({ catid, title, contents }) {
   let contentList;
-  if (contents) contentList = <ContentList contents = {contents} />;
+  if (contents) contentList = <ContentList contents={contents} />;
 
   return (
     <div>
