b144631ad21d82b1b582606c2848156d08c12a2b
Tyler Shelton <tdshelton@gmail.com>
2024-07-02 12:28:30 -0700

    Adapt component to React Router's workflow

--- client/components/Article.jsx
+++ client/components/Article.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useLoaderData, useMatch } from 'react-router-dom';
 
-const Article = ({ title, description, pubdate, url }) => {
+export async function loader ({ request }) {
+  console.log('fetching:', request.url);
+  const url = new URL(request.url);
+  return await fetch(url);
+}
+
+export default function Article ({ title, description, pubdate, url }) {
+  const article = useLoaderData();
+  console.log(article);
+  const match = useMatch();
+  console.log(match);
   return (
     <section className='article'>
       <article>
@@ -12,6 +23,4 @@ const Article = ({ title, description, pubdate, url }) => {
       </article>
     </section>
   );
-};
-
-export default Article;
+}
