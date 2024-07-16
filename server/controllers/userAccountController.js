cc9d1c7174814c4359f91d741478ccb467b711d1
Tyler Shelton <tdshelton@gmail.com>
2024-07-15 22:57:39 -0700

    Remove debug code

--- server/controllers/userAccountController.js
+++ server/controllers/userAccountController.js
@@ -3,18 +3,11 @@ const User = require('../models/user');
 module.exports = {
   createUser: async (req, res, next) => {
     try {
-      const {username, password} = req.body;
-      if (Buffer.from(username, 'utf8').toString('utf8') !== username || Buffer.from(password, 'utf8').toString('utf8') !== password) {
-        throw new Error('Invalid UTF-8 encoding detected');
-      }
-
-      console.log(req.body);
-      const result = await User.create(req.body.username, req.body.password);
-      console.log(result);
-      next();
+      await User.create(req.body.username, req.body.password);
+      return next();
     }
     catch (err) {
-      next(err);
+      return next(err);
     }
   },
 };
