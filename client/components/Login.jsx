3897b972f26225e72fc2f8df7bf2857852816c92
Tyler Shelton <tdshelton@gmail.com>
2024-07-15 20:53:10 -0700

    Upload login data as formdata

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -3,41 +3,54 @@ import { Form, redirect } from 'react-router-dom';
 
 export async function action ({ request }) {
   const formData = await request.formData();
-  console.log(formData);
+
   const response = await fetch('/auth/login', {
     method: 'post',
-    body: formData,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams(formData).toString(),
   });
+
   console.log(response);
-  return redirect('/');
+  const result = await response.json();
+  console.log(result);
+  if (response.ok) {
+    return redirect('/');
+  } else {
+    return redirect('/login');
+  }
 }
 
 export default function Login () {
   return (
-    <Form method='post' id='login-form'>
-      <section>
-        <label>
-          Username
-          <input
-            placeholder='Username'
-            type='text'
-            name='username'
-            required
-          />
-        </label>
-      </section>
-      <section>
-        <label>
-          Password
-          <input
-            placeholder='password'
-            type='password'
-            name='password'
-            required
-          />
-        </label>
-      </section>
-      <button type='submit'>Sign In</button>
-    </Form>
+    <section>
+      <h2>Sign In</h2>
+      <Form method='post' id='login-form'>
+        <section>
+          <label>
+            Username
+            <input
+              placeholder='Username'
+              type='text'
+              name='username'
+              required
+            />
+          </label>
+        </section>
+        <section>
+          <label>
+            Password
+            <input
+              placeholder='password'
+              type='password'
+              name='password'
+              required
+            />
+          </label>
+        </section>
+        <button type='submit'>Sign In</button>
+      </Form>
+    </section>
   );
 }
