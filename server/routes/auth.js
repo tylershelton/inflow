8ad03a41bb96e0c9112a6914075aa8ce4568ddab
Tyler Shelton <tdshelton@gmail.com>
2024-07-15 20:54:08 -0700

    Use User model to access db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,32 +1,33 @@
-const crypto = require('crypto');
-const express = require('express');
-const passport = require('passport');
+const crypto        = require('crypto');
+const express       = require('express');
+const passport      = require('passport');
 const LocalStrategy = require('passport-local');
-const router = express.Router();
+const router        = express.Router();
 
-const pool = require('../lib/db');
+const User = require('../models/user');
+const userAccountController = require('../controllers/userAccountController');
 
-passport.use(new LocalStrategy(function verify(username, password, callback) {
-  pool.query(
-    'SELECT * FROM users WHERE username = ?',
-    [ username ],
-    function (err, row) {
+passport.use(new LocalStrategy(async function verify(username, password, callback) {
+  try {
+    const user = await User.get(username);
+    console.log('found user in db:', user);
+    if (!user) return callback(null, false, { message: 'Incorrect username or password.' });
+
+    crypto.pbkdf2(password, user.salt, 31000, 32, 'sha256', function (err, hashedPassword) {
       if (err) return callback(err);
-      if (!row) return callback(null, false, { message: 'Incorrect username or password.' });
-
-      crypto.pbkdf2(password, row.salt, 31000, 32, 'sha256', function (err, hashedPassword) {
-        if (err) return callback(err);
-        if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+      if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
           return callback(null, false, { message: 'Incorrect username or password.' });
         }
-        return callback(null, row);
-      });
+      return callback(null, user);
     });
+  }
+  catch (err) {
+    return callback(err);
+  }
 }));
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
+  failureMessage: true,
 }));
 
 module.exports = router;
