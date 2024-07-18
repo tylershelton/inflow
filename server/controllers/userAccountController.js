d7bfae0888f54e45a0003803523edfc52cd82b3a
Tyler Shelton <tdshelton@gmail.com>
2024-07-17 22:47:49 -0700

    Set up error handling in auth controller and model logic

--- server/controllers/userAccountController.js
+++ server/controllers/userAccountController.js
@@ -1,8 +1,14 @@
 const User = require('../models/user');
+const { ValidationError } = require('../lib/error/errors');
 
 module.exports = {
   createUser: async (req, res, next) => {
-    await User.create(req.body.username, req.body.password);
+    const { username, password, passwordConfirm } = req.body;
+    
+    if (password !== passwordConfirm)
+      throw new ValidationError('Passwords do not match.');
+
+    await User.create(username, password);
     return next();
   },
 };
--- server/lib/error/errors.js
+++ server/lib/error/errors.js
@@ -1,10 +1,17 @@
 class AppError extends Error {
-  constructor (message, statusCode) {
+  constructor (message, statusCode, cause) {
     super(message);
     this.statusCode = statusCode;
+    this.cause = cause;
     // capture the stack at this exact moment, and remove
     // this constructor function's frame from it
     Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class ValidationError extends AppError {
+  constructor(message = 'Validation Error') {
+    super(message, 400);
   }
 }
 
