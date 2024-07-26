bd13c59f4a4e948382b885f7db87a221788fb9eb
Tyler Shelton <tdshelton@gmail.com>
2024-07-25 22:54:00 -0700

    Switch from `fetcher.Form` to a "regular" React Router `Form`

--- client/components/LogoutButton.jsx
+++ client/components/LogoutButton.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { useFetcher } from 'react-router-dom';
+import { Form } from 'react-router-dom';
 
 export default function LogoutButton () {
-  const fetcher = useFetcher();
-
   return (
-    <fetcher.Form method="post" action="/logout">
+    <Form method="post" action="/logout">
       <button type="submit">
         Log Out
       </button>
-    </fetcher.Form>
+    </Form>
   );
 }
