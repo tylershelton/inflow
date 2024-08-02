fb7de282b7bcc2e1fa9c4cf37a39b4952f5f3817
Tyler Shelton <tdshelton@gmail.com>
2024-08-01 21:24:21 -0700

    Consolidate session table into schema file

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -33,3 +33,14 @@ CREATE TABLE IF NOT EXISTS user_account (
     created_at      timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- table for persisting user sessions
+CREATE TABLE user_session (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+);
+
+ALTER TABLE "user_session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE;
+
+CREATE INDEX "IDX_session_expire" ON "user_session" ("expire");
