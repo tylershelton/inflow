0fdded22f6870a4bd50fb23df1f07896a741b6d9
Tyler Shelton <tdshelton@gmail.com>
2024-08-03 14:08:59 -0700

    Provide cleaner STDOUT messaging

--- scripts/db_ops/reset.sh
+++ scripts/db_ops/reset.sh
@@ -4,8 +4,15 @@
 
 ensure_docker_is_running
 
+echo "==> stopping the \`db\` service container..."
 docker compose -f compose.dev.yml stop db > /dev/null 2>&1
+
+echo "==> deleting the \`db\` service container..."
 docker compose -f compose.dev.yml rm -f db > /dev/null 2>&1
+
+echo "==> deleting the database volume..."
 docker volume rm inflow-dev_inflowdb > /dev/null 2>&1
+
+echo "==> rebuilding/repopulating database..."
 docker compose -f compose.dev.yml up -d db > /dev/null 2>&1
 docker compose -f ./docker-compose.dev.yml up -d
