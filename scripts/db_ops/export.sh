b8a1cf07a2aa0dac6433073d70151c0a796810e2
Tyler Shelton <tdshelton@gmail.com>
2024-08-03 11:29:07 -0700

    Set a standard cwd for db subcommands

--- scripts/db_ops/export.sh
+++ scripts/db_ops/export.sh
@@ -2,12 +2,11 @@
 
 . "$(dirname "$0")/utils.sh"
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
-out_file="${REPO_ROOT}/database/test_data/dev_dataset.sql"
-
 ensure_docker_is_running
+
+out_file="./database/test_data/dev_dataset.sql"
 
 # update the dataset used to populate the database in non-production environments
 # by dumping its current state to a file
-docker compose -f "${REPO_ROOT}/docker-compose.dev.yml" exec db \
+docker compose -f "./docker-compose.dev.yml" exec db \
     /bin/bash -c 'pg_dump -a -U inflow -d inflow' > "$out_file"
--- scripts/db_ops/reset.sh
+++ scripts/db_ops/reset.sh
@@ -4,9 +4,6 @@
 
 ensure_docker_is_running
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
-cd $REPO_ROOT
-
 docker compose -f ./docker-compose.dev.yml stop db
 docker compose -f ./docker-compose.dev.yml rm -f db
 docker volume rm inflow-dev_inflowdb
