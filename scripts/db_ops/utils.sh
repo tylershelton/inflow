5a67fe156bcfdcc93ac4145094cd81f41576cb78
Tyler Shelton <tdshelton@gmail.com>
2024-08-03 14:08:27 -0700

    Add function to check whether a specific service container is up

--- scripts/db_ops/utils.sh
+++ scripts/db_ops/utils.sh
@@ -5,3 +5,11 @@ ensure_docker_is_running() {
     fi
     return 0
 }
+
+docker_service_is_running() {
+    local service_name="$1"
+    if docker compose -f compose.dev.yml ps --services --filter "status=running" | grep -q "^${service_name}$"; then
+        return 0 # service is running
+    fi
+    return 1 # service is not running
+}
