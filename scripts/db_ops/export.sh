bdad3b4bb62082de166f8ebe4deb2e5e1b798909
Tyler Shelton <tdshelton@gmail.com>
2024-08-05 00:19:04 -0700

    Use an env var to pair a compose file with an environment

--- scripts/db_ops/export.sh
+++ scripts/db_ops/export.sh
@@ -9,16 +9,16 @@ docker_service_is_running db; db_was_running=$?
 
 if [ "$db_was_running" -eq 1 ]; then
     echo "==> starting the \`db\` service container..."
-    docker compose -f compose.dev.yml up -d db > /dev/null 2>&1
+    docker compose -f "$PROJECT_COMPOSE_FILE" up -d db > /dev/null 2>&1
 fi
 
 # update the dataset used to populate the database in non-production environments
 # by dumping its current state to a file
 echo "==> dumping database tables..."
-docker compose -f compose.dev.yml exec db \
+docker compose -f "$PROJECT_COMPOSE_FILE" exec db \
     /bin/bash -c 'pg_dump -a -U inflow -d inflow' > "$out_file"
 
 if [ "$db_was_running" -eq 1 ]; then
     echo "==> stopping \`db\` service container, as it was not running before export."
-    docker compose -f compose.dev.yml stop db > /dev/null 2>&1
+    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
 fi
--- scripts/db_ops/reset.sh
+++ scripts/db_ops/reset.sh
@@ -7,18 +7,18 @@ ensure_docker_is_running
 db_was_running=$(docker_service_is_running db)
 
 echo "==> stopping the \`db\` service container..."
-docker compose -f compose.dev.yml stop db > /dev/null 2>&1
+docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
 
 echo "==> deleting the \`db\` service container..."
-docker compose -f compose.dev.yml rm -f db > /dev/null 2>&1
+docker compose -f "$PROJECT_COMPOSE_FILE" rm -f db > /dev/null 2>&1
 
 echo "==> deleting the database volume..."
 docker volume rm inflow-dev_inflowdb > /dev/null 2>&1
 
 echo "==> rebuilding/repopulating database..."
-docker compose -f compose.dev.yml up -d db > /dev/null 2>&1
+docker compose -f "$PROJECT_COMPOSE_FILE" up -d db > /dev/null 2>&1
 
 if ! $db_was_running; then
     echo "==> stopping \`db\` service container, as it was not running before reset."
-    docker compose -f compose.dev.yml stop db > /dev/null 2>&1
+    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
 fi
