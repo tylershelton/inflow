dac13f1aa86ce6c70bbff81b2f0568f73ec570b1
Tyler Shelton <tdshelton@gmail.com>
2024-08-04 18:17:14 -0700

    Scope internal function variables to the function by running the function logic in a subshell

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 ensure_docker_is_running() {
     if ! docker info > /dev/null 2>&1; then
         echo "ERROR: Docker does not appear to be running. Please start the docker service and try again."
@@ -7,20 +9,26 @@ ensure_docker_is_running() {
 }
 
 docker_service_is_running() {
-    local service_name="$1"
-    if docker compose -f compose.dev.yml ps --services --filter "status=running" | grep -q "^${service_name}$"; then
-        return 0 # service is running
-    fi
-    return 1 # service is not running
+    (
+        service_name="$1"
+        if docker compose -f compose.dev.yml ps --services --filter "status=running" | grep -q "^${service_name}$"; then
+            exit 0 # service is running
+        fi
+        exit 1 # service is not running
+    )
+    return $?
 }
 
 in_array() {
-    local val="$1"
-    local arr="$2"
-    for elem in $arr; do
-        if [ "$elem" = "$val" ]; then
-            return 0
-        fi
-    done
-    return 1
+    (
+        val="$1"
+        arr="$2"
+        for elem in $arr; do
+            if [ "$elem" = "$val" ]; then
+                exit 0
+            fi
+        done
+        exit 1
+    )
+    return $?
 }
