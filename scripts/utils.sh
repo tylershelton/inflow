62ea5afc7bebf131d947264a23f34d1095264fda
Tyler Shelton <tdshelton@gmail.com>
2024-08-04 18:03:36 -0700

    Handle attempt to switch to an already active env

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -13,3 +13,14 @@ docker_service_is_running() {
     fi
     return 1 # service is not running
 }
+
+in_array() {
+    local val="$1"
+    local arr="$2"
+    for elem in $arr; do
+        if [ "$elem" = "$val" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
