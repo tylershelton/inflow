c941e49019e0c96de50b4f0da6c99e8327d720ef
Tyler Shelton <tdshelton@gmail.com>
2024-08-18 12:09:32 -0700

    Tabbing/formatting cleanup

--- database/migrations/migrate/20240809-init.sql
+++ database/migrations/migrate/20240809-init.sql
@@ -1,25 +1,25 @@
 CREATE TABLE IF NOT EXISTS category (
-    id                         SERIAL PRIMARY KEY,
-    title       varchar(80)    NOT NULL UNIQUE
+    id                              SERIAL PRIMARY KEY,
+    title           varchar(80)     NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS feed (
-    id                          SERIAL PRIMARY KEY,
-    url         varchar(255)    NOT NULL UNIQUE,
-    title       varchar(255)    NOT NULL,
-    description text,
-    category_id integer         REFERENCES category(id)
+    id                              SERIAL PRIMARY KEY,
+    url             varchar(255)    NOT NULL UNIQUE,
+    title           varchar(255)    NOT NULL,
+    description     text,
+    category_id     integer         REFERENCES category(id)
 );
 
 CREATE TABLE IF NOT EXISTS feeditem (
-    id                          SERIAL PRIMARY KEY,
-    title       varchar(255),
-    description text,
-    url         varchar(255)    UNIQUE,
-    pubDate     timestamp,
-    archived    boolean         DEFAULT false,
-    feed_id     integer         REFERENCES feed(id),
-    category_id integer         REFERENCES category(id),
+    id                              SERIAL PRIMARY KEY,
+    title           varchar(255),
+    description     text,
+    url             varchar(255)    UNIQUE,
+    pubDate         timestamp,
+    archived        boolean         DEFAULT false,
+    feed_id         integer         REFERENCES feed(id),
+    category_id     integer         REFERENCES category(id),
     
     CONSTRAINT title_or_description CHECK (title IS NOT NULL OR description IS NOT NULL)
 );
@@ -36,9 +36,9 @@ CREATE TABLE IF NOT EXISTS user_account (
 
 -- table for persisting user sessions
 CREATE TABLE IF NOT EXISTS user_session (
-  "sid"             varchar         NOT NULL COLLATE "default",
-  "sess"            json            NOT NULL,
-  "expire"          timestamp(6)    NOT NULL
+    sid             varchar         NOT NULL COLLATE "default",
+    sess            json            NOT NULL,
+    expire          timestamp(6)    NOT NULL
 );
 
 DO $$
