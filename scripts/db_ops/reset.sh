49028da3a5675a464f2bdfaa52d780f2262408fb
Tyler Shelton <tdshelton@gmail.com>
2024-08-18 14:51:38 -0700

    Only make `reset.sh` responsible for deleting the db data volume

--- scripts/db_ops/reset.sh
+++ scripts/db_ops/reset.sh
@@ -6,19 +6,17 @@ ensure_docker_is_running
 
 db_was_running=$(docker_service_is_running db)
 
-echo "==> stopping the \`db\` service container..."
-docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
-
-echo "==> deleting the \`db\` service container..."
-docker compose -f "$PROJECT_COMPOSE_FILE" rm -f db > /dev/null 2>&1
+if $db_was_running; then
+    echo "==> stopping the \`db\` service container..."
+    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
+fi
 
 echo "==> deleting the database volume..."
 docker volume rm inflow-dev_inflowdb > /dev/null 2>&1
 
-echo "==> rebuilding/repopulating database..."
-docker compose -f "$PROJECT_COMPOSE_FILE" up -d db > /dev/null 2>&1
-
-if ! $db_was_running; then
-    echo "==> stopping \`db\` service container, as it was not running before reset."
-    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
+if $db_was_running; then
+    echo "==> restarting \`db\` service container..."
+    docker compose -f "$PROJECT_COMPOSE_FILE" start db > /dev/null 2>&1
 fi
+
+exit 0
