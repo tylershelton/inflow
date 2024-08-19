a219fbecd7cdc16978e7ddcdfdf5747c598b66d0
Tyler Shelton <tdshelton@gmail.com>
2024-08-19 00:05:36 -0700

    Capture db service check status correctly

--- scripts/db_ops/reset.sh
+++ scripts/db_ops/reset.sh
@@ -2,21 +2,25 @@
 
 . "$(dirname "$0")/../utils.sh"
 
+DB_VOLUME='inflow-dev_inflowdb'
+
 ensure_docker_is_running
 
-db_was_running=$(docker_service_is_running db)
+docker_service_is_running db; db_was_running=$?
 
-if $db_was_running; then
+if [ "$db_was_running" -eq 0 ]; then
     echo "==> stopping the \`db\` service container..."
-    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null 2>&1
+    docker compose -f "$PROJECT_COMPOSE_FILE" stop db > /dev/null
 fi
 
-echo "==> deleting the database volume..."
-docker volume rm inflow-dev_inflowdb > /dev/null 2>&1
+if docker volume ls -q | grep "^${DB_VOLUME}$"; then
+    echo "==> deleting the database volume..."
+    docker volume rm "${DB_VOLUME}" > /dev/null
+fi
 
-if $db_was_running; then
+if [ "$db_was_running" -eq 0 ]; then
     echo "==> restarting \`db\` service container..."
-    docker compose -f "$PROJECT_COMPOSE_FILE" start db > /dev/null 2>&1
+    docker compose -f "$PROJECT_COMPOSE_FILE" start db > /dev/null
 fi
 
 exit 0
