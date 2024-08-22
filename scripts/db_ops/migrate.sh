4d0da9dd446918a834d220ba3f589df8ffc44eeb
Tyler Shelton <tdshelton@gmail.com>
2024-08-21 21:55:54 -0700

    Wait for postgres to come fully up before trying to execute SQL

--- scripts/db_ops/migrate.sh
+++ scripts/db_ops/migrate.sh
@@ -121,8 +121,15 @@ exec_psql() {
 }
 
 if [ $db_was_running -eq 1 ]; then
+
     echo "==> starting the \`db\` service container..."
     docker compose -f "$PROJECT_COMPOSE_FILE" up -d db > /dev/null
+
+    echo "==> waiting for postgres to start..."
+    while docker compose -f "$PROJECT_COMPOSE_FILE" exec db pg_isready -U "$INFLOW_DB_USER"; do
+        sleep 1
+    done
+
 fi
 
 # init migration table
